34c699d94a906ac6120ca11c01a3a79cb7e0059b
Leiramarie San Buenaventura <lsanbuenaventura.k11937984@umak.edu.ph>
2025-10-09 01:19:03 +0800

    Fixed Dark and Light Mode, Login, Signup, Dashboard template

---
+++ app/View/Components/layouts/AdminDashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\View\Components\Layouts;
+
+use Illuminate\View\Component;
+
+class AdminDashboard extends Component
+{
+    public function render()
+    {
+        return view('components.layouts.admin-dashboard');
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,46 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class', // Enables manual dark mode toggling
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",  // Laravel Blade templates
+        "./resources/**/*.js",         // Any JS inside your resources folder
+        "./resources/**/*.vue",        // (if using Vue)
+        './node_modules/flowbite/**/*.js',
     ],
 
     theme: {
         extend: {
+            colors: {
+                primary: {
+                    light: '#93C5FD', //White
+                    DEFAULT: '#2A6DFA', //Bright Blue
+                    dark: '#081032', //Dark Blue
+                },
+                buttons: {
+                    lightHover: '#D4D4D4', //light colored button
+                    defaultHover: '#003FC5', // blue colored button
+                    darkHover: '#002676', //dark colored button
+                    defaultDisabled: '#D9D9D9'
+                },
+                badges: {
+                    complete: '#2FBC00',
+                    progress: '#FFBF00',
+                    incomplete: '#FF1823'
+                },
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['FamiljenGrotesk', ...defaultTheme.fontFamily.sans], // use your font as default
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('flowbite/plugin'),
+    ],
 };
