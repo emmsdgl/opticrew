1af4e8909f843e5882672a563e40160d650397fc
Leiramarie San Buenaventura <lsanbuenaventura.k11937984@umak.edu.ph>
2026-02-08 23:17:50 +0800

    Fixed revised admin lists - Fully functional

--- resources/views/components/admin-request-list-item.blade.php
+++ resources/views/components/admin-request-list-item.blade.php
@@ -93,8 +93,8 @@
                 <span class="md:hidden text-xs font-semibold text-gray-500 dark:text-gray-400 mb-1 mr-2">Action:</span>
                 <button
                     @click="$dispatch('open-request-modal', { index: {{ $index }} })"
-                    class="w-full px-4 py-2 rounded-lg text-sm font-medium transition-all duration-200 bg-blue-600 text-white hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600">
-                    View
+                    class="text-sm font-medium text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 transition-colors">
+                    <i class="fa-regular fa-eye mr-1 text-xs"></i> View
                 </button>
             </div>
         </div>
